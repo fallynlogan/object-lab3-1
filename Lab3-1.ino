84e99f01e6ee43155a3be2c939c5f6ee42c7c684
fallynlogan <49659834+fallynlogan@users.noreply.github.com>
2019-09-24 13:17:17 -0600

    Add files via upload

---
+++ Lab3-1.ino
@@ -0,0 +1,26 @@
+//button connected to pin 7 
+//led connected to pin 8 
+  int buttonPin = 7;
+  int LEDPin = 8;
+void setup() {
+  // put your setup code here, to run once:
+  //set digital pins as inputs and outputs 
+  pinMode(buttonPin, INPUT);
+  pinMode(LEDPin, OUTPUT);
+  
+}
+
+void loop() {
+  //if button is pressed turn leds on otherwise off 
+  if(digitalRead(buttonPin)==1){
+    digitalWrite(LEDPin, HIGH);
+  }else{
+    digitalWrite(LEDPin, LOW);
+  }
+  //prints each piece on info from board to ternminal on new line 
+  Serial.println(digitalRead(buttonPin));
+  //slows down print rate (in milliseconds) 
+  delay(250);
+  
+
+}
